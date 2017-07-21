4636ac0372855549076cb6e5901cb050c3fb2441
Dorothy  Eves <dorothyeves7@gmail.com>
2017-07-21 00:06:51 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,14 +1,29 @@
-function forLoop(array){
-var i;
 
-for (let i = 0; i < 25; i++){
-  if (i === "1"){
-    console.log("I am" + i + "strange loop.");
+function forLoop(array){
+var array = [];
+
+
+for (var i = 0; i < 25; i++){
+  if (var i = 1){
+    return ["I am ${i} strange loop"];
   }
   else {
-    console.log("I am" + ${i} + "strange loops.")
+    return ["I am ${i} strange loops."];
   }
+  }
+  return [array];
 
 
+
+/*
+function whileLoop(n){
+  return Math.random() = 0
+  let countdown = 10;
+
+  while (whileLoop()){
+    console.log(--countdown);
+    return('done');
+   }
 
 }
+*/
